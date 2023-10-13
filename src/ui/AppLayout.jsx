e8342178d880a5ce188c5b3ecbc336e50623ee1f
osoff <anatolywebdev@gmail.com>
2023-10-14 01:33:57 +0200

    add:router&&loader

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,5 +1,24 @@
+import Header from "./Header";
+import Loader from "./Loader";
+
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+
 function AppLayout() {
-  return <div></div>;
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+  console.log(navigation);
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+      <main>
+        <h1>Content</h1>
+        <Outlet />
+      </main>
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <Link to="/">Fast React Pizza Go</Link>
+    </div>
+  );
+}
+
+export default Header;
